17ff7c1d2909394ed682d443f363e44850fa6116
asianburpgirl <estherrrlychee@gmail.com>
2023-02-24 17:33:33 +0800

    Added buttons to list items

--- src/views/NotificationPage.vue
+++ src/views/NotificationPage.vue
@@ -16,18 +16,23 @@
                 <ion-list>
                     <ion-item>
                         <ion-label>Pokémon Yellow</ion-label>
+                        <ion-button>Start Challenge</ion-button>
                     </ion-item>
                     <ion-item>
                         <ion-label>Mega Man X</ion-label>
+                        <ion-button>Start Challenge</ion-button>
                     </ion-item>
                     <ion-item>
                         <ion-label>The Legend of Zelda</ion-label>
+                        <ion-button>Start Challenge</ion-button>
                     </ion-item>
                     <ion-item>
                         <ion-label>Pac-Man</ion-label>
+                        <ion-button>Start Challenge</ion-button>
                     </ion-item>
                     <ion-item>
                         <ion-label>Super Mario World</ion-label>
+                        <ion-button>Start Challenge</ion-button>
                     </ion-item>
                 </ion-list>
             </ion-grid>
