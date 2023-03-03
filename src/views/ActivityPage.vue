cf487b869a8c01061033a219f28c03cac600ba3d
Eaint2000-droid <79896968+Eaint2000-droid@users.noreply.github.com>
2023-03-03 14:41:11 +0800

    add more details on Activity Page

--- src/views/ActivityPage.vue
+++ src/views/ActivityPage.vue
@@ -14,18 +14,53 @@
       <!-- can make use of ion-grid to contain your stuff -->
       <ion-grid>
         <ion-card style="width: 340px; height: 600px; background-color: #Abaad8;">
-          <ion-img src="../assets/deepbreath.png" 
-          style="width: 200px; height: 200px; margin-left:70px; margin-top:90px"></ion-img>
+          <ion-grid>
+            <ion-row>
+              <ion-col size="3">
+                <ion-img src="../assets/timer.png"
+                  style="width: 45px; height: 45px; margin-left:170px; margin-top:20px"></ion-img>
+              </ion-col>
+              <ion-col size="6">
+                <ion-input
+                  style="border-radius:1rem;width:50px;background-color:white;
+                  margin-left:140px; margin-top:25px; color: black; text-align: center;"></ion-input>
+              </ion-col>
+              <ion-col size="3">
+                <p style="margin-top:39px; margin-left:32px; font-weight:bold; color:white;">mins</p>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+          <ion-grid>
+            <ion-row>
+              <ion-col size="3">
+                <ion-img src="../assets/left-arrow.png"
+                  style="width: 50px; height: 50px; margin-left:-10px; margin-top:150px">
+                </ion-img>
+              </ion-col>
+              <ion-col size="6">
+                <ion-img src="../assets/deepbreath.png"
+                  style="width: 200px; height: 200px; margin-left:-20px; margin-top:50px">
+                </ion-img>
+              </ion-col>
+              <ion-col size="3">
+                <ion-img src="../assets/right-arrow.png"
+                  style="width: 50px; height: 50px; margin-left:30px; margin-top:150px">
+                </ion-img>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
           <ion-card-header>
-            <ion-card-subtitle style="margin-left:45px; margin-top:40px;font-weight: bold; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 0.9rem;">Petals flowing in the wind</ion-card-subtitle>
+            <ion-card-subtitle
+              style="margin-left:45px; margin-top:40px;font-weight: bold; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 0.9rem;">
+              Petals flowing in the wind</ion-card-subtitle>
           </ion-card-header>
           <ion-card-content>
             <p style="color:#F7f7ec ; font-weight: bold;text-align: center;">
               Relax your mind, body, and soul with each breath.
             </p>
             <br>
-            <ion-button color="pink" style="background-color: pink; border-radius: 5vi; justify-self: center; display:flex;
-            color:#222222; font-weight:bold">
+            <ion-button color="#FFB6C1" style="background-color:#FFB6C1; border-radius: 5vi; justify-self: center; display:flex;
+            font-weight:bold; color:#1a1a1a">
               Start Breathing</ion-button>
           </ion-card-content>
         </ion-card>
@@ -44,4 +79,6 @@ import {
   IonGrid,
   IonImg,
 } from "@ionic/vue";
+
+
 </script>
