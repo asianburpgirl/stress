9ce98014143889fd07b92b613179de5d971bf653
qiaokoli <105535229+qiaokoli@users.noreply.github.com>
2023-03-06 14:54:50 +0800

    Edited the hone page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,92 +1,121 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Home</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true" ion-padding>
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Home</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <!-- can make use of ion-grid to contain your stuff -->
-      <ion-grid>
-        <ion-card style="width: 340px; height: 600px; background-color: white;">
-          <ion-grid>
-            <ion-row>
-              <ion-img src="../assets/logo and bubbles.png" style="width: 100%; height: 100%">
-              </ion-img>
-            </ion-row>
-          </ion-grid>
-          <ion-card-header>
-            <ion-card-subtitle
-              style="text-align: center; margin-top: -15px; font-weight: bold; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 1.2rem">
-              <span style="color: #5678AB">Stress</span><span style="color:#D36FA5">Less</span>
-            </ion-card-subtitle>
-          </ion-card-header>
-          <ion-grid>
-            <ion-row>
-              <ion-card-subtitle
-                style="color: black; text-align: center; width: 100%; font-weight: bold; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 0.9rem">
-                Scan with Singpass app
-              </ion-card-subtitle>
-              <ion-card-subtitle
-                style="color: black; text-align: center; width: 100%; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 0.9rem">
-                to log in
-              </ion-card-subtitle>
-            </ion-row>
-            <ion-row>
-              <a href="/">
-                <ion-img src="../assets/singpass.png" 
-                style="width: 55%; height: auto; display: block; margin-top: 5%; margin-left: auto; margin-right: auto">
-                </ion-img>
-              </a>
-            </ion-row>
-            <ion-row>
-              <ion-button href="/" size="small" style="display: block; margin-top: 10px; margin-left: auto; margin-right: auto; text-transform: none">
-                <ion-icon slot="start" :icon="lockClosed"></ion-icon>
-                Use password login instead
-              </ion-button>
-            </ion-row>
-            <ion-row>
-                <ion-card-subtitle
-                  style="color: gray; text-align: center; width: 100%; margin-top: 5px; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 0.2rem">
-                  Don't have the Singpass app?
-                </ion-card-subtitle>
-                <ion-card-subtitle
-                  style="text-align: center; width: 100%; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 0.2rem">
-                  <a href="/" style="color: red; text-decoration: none">Download now</a>
-                </ion-card-subtitle>
-              </ion-row>
-          </ion-grid>
-        </ion-card>
-      </ion-grid>
-    </ion-content>
-  </ion-page>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Home</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content :fullscreen="true" ion-padding>
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-title size="large">Home</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <!-- can make use of ion-grid to contain your stuff -->
+            <ion-grid>
+                <ion-card style="width: 340px; height: 600px; background-color: white;">
+                    <ion-grid>
+                        <ion-row>
+                            <ion-card-header
+                                style="color: #5678AB; text-align: center; font-weight: bold; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 1.2rem; margin-left: 25px; margin-top:50px">
+                                Hello, Anna!
+                            </ion-card-header>
+                            <ion-card style="display: block; margin: auto; width: 275px; border-radius: 10px">
+                                <ion-card-header>
+                                    <ion-card-title
+                                        style="font-weight: bold; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 1rem">
+                                        How are you feeling today?
+                                    </ion-card-title>
+                                    <ion-card-subtitle style="font-size: 0.6rem">
+                                        Here's a summary of your health statistics:
+                                    </ion-card-subtitle>
+                                </ion-card-header>
+                                <ion-card-content>
+                                    <ion-grid>
+                                        <ion-row>
+                                            <ion-col>
+                                                <ion-card style="width: 70%; margin-left: -10px">
+                                                    <ion-img src="../assets/heartrate.png"
+                                                        style="width: 25%; float: left; margin-right: 5px">
+                                                    </ion-img>
+                                                    <ion-card-subtitle style="font-size: 0.3rem">
+                                                        Heart rate: 75 bpm
+                                                    </ion-card-subtitle>
+                                                </ion-card>
+                                            </ion-col>
+                                        </ion-row>
+                                    </ion-grid>
+                                </ion-card-content>
+                            </ion-card>
+                        </ion-row>
+                        <ion-row>
+                            <ion-card-subtitle
+                                style="font-size: 0.6rem; font-weight: bold; text-align: center; margin: 30px auto">
+                                Here are some recommended therapies for you:
+                            </ion-card-subtitle>
+                        </ion-row>
+                        <ion-row style="margin: -20px auto; width: 95%">
+                            <ion-col>
+                                <ion-card style="height: 100px; border-radius: 10px; background-color: #F2F7FF">
+                                    <ion-img src="../assets/deepbreathing.png"
+                                        style="width: 30%; display: block; margin: 15px auto">
+                                    </ion-img>
+                                    <ion-card-subtitle style="text-align: center; font-size: 0.6rem; margin-top: 10px">
+                                        Deep breathing
+                                    </ion-card-subtitle>
+                                </ion-card>
+                                <ion-card
+                                    style="height: 100px; margin-top: 20px; border-radius: 10px; background-color: #F2F7FF">
+                                    <ion-img src="../assets/reading.png"
+                                        style="width: 30%; display: block; margin: 15px auto">
+                                    </ion-img>
+                                    <ion-card-subtitle style="text-align: center; font-size: 0.6rem; margin-top: 10px">
+                                        Reading (30 min)
+                                    </ion-card-subtitle>
+                                </ion-card>
+                            </ion-col>
+                            <ion-col>
+                                <ion-card style="height: 100px; border-radius: 10px; background-color: #F2F7FF">
+                                    <ion-img src="../assets/gardening.png"
+                                        style="width: 30%; display: block; margin: 15px auto">
+                                    </ion-img>
+                                    <ion-card-subtitle style="text-align: center; font-size: 0.6rem; margin-top: 10px">
+                                        Gardening
+                                    </ion-card-subtitle>
+                                </ion-card>
+                                <ion-card
+                                    style="height: 100px; margin-top: 20px; border-radius: 10px; background-color: #F2F7FF">
+                                    <ion-img src="../assets/yoga.png" style="width: 30%; display: block; margin: 15px auto">
+                                    </ion-img>
+                                    <ion-card-subtitle style="text-align: center; font-size: 0.6rem; margin-top: 10px">
+                                        Doing yoga (15 min)
+                                    </ion-card-subtitle>
+                                </ion-card>
+                            </ion-col>
+                        </ion-row>
+                    </ion-grid>
+                </ion-card>
+            </ion-grid>
+        </ion-content>
+    </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 
-import { IonButton, IonIcon } from '@ionic/vue';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
 import { defineComponent } from 'vue';
-import { lockClosed } from 'ionicons/icons';
 </script>
 
 <style scoped>
-ion-button {
-  --background: white;
-  --color: gray;
-  --color-hover: black;
-  --border-color: gray;
-  --border-style: solid;
-  --border-width: 1px;
-  --bow-shadow: none;
-  --text-transform: none;
-  --padding-top: 10px;
-  --padding-bottom: 10px;
+/* iOS places the subtitle above the title */
+ion-card-header.ios {
+    display: flex;
+    flex-flow: column-reverse;
+}
+
+ion-card-title {
+    --color: gray
 }
 </style>
